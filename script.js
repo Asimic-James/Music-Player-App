53b053eca7b3e04c5d7a4b4f56393aa0878b925e
Asimic James Uka <jayasimic@gmail.com>
2024-01-05 22:04:37 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -98,6 +98,7 @@ const playSong = (id) => {
   userData.currentSong = song;
   playButton.classList.add("playing");
 
+  highlightCurrentSong();
   audio.play();
 };
 
@@ -119,7 +120,7 @@ const playNextSong = () => {
   }
 };
 
-const playPreviousSong = () => {
+const playPreviousSong = () =>{
    if (userData?.currentSong === null) return;
    else {
     const currentSongIndex = getCurrentSongIndex();
@@ -129,13 +130,24 @@ const playPreviousSong = () => {
    }
 };
 
+const setPlayerDisplay = () => {
+  const playingSong = document.getElementById("player-song-title");
+  const songArtist = document.getElementById("player-song-artist");
+const currentTitle = userData?.currentSong?.title;
+const currentArtist = userData?.currentSong?.artist;  
+};
+
 const highlightCurrentSong = () => {
   const playlistSongElements = document.querySelectorAll(".playlist-song");
   const songToHighlight = document.getElementById(
     `song-${userData?.currentSong?.id}`
   );
-  
 
+  playlistSongElements.forEach((songEl) => {
+    songEl.removeAttribute("aria-current");
+  });
+
+  if (songToHighlight) songToHighlight.setAttribute("aria-current", "true");
 };
 
 const renderSongs = (array) => {
